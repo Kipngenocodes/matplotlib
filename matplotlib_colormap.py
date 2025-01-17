1e114601cd8f47e075b9741e1b942b24e121171b
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-16 22:10:14 -0800

    working with colormaps

--- matplotlib_colormap.py
+++ matplotlib_colormap.py
@@ -1,10 +1,24 @@
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import matplotlib
-import numpy as np 
+import numpy as np
+from matplotlib.colors import ListedColormap
 
-# Checking a colormap
-# print(list(matplotlib.colormaps))
+# List of colormap names to iterate through
+colormap_names = ['viridis', 'plasma', 'inferno', 'magma', 'cividis']
 
-# accessing the colormaps 
-viridis = matplotlib.colormaps['viridis'].resampled(8)
-print(viridis)
+# Generate random data for visualization
+np.random.seed(42)
+data = np.random.rand(10, 10)  # Example 2D data
+
+# Create a figure and axes for subplots
+fig, axes = plt.subplots(1, len(colormap_names), figsize=(15, 4), constrained_layout=True)
+
+# Loop through the colormap names and plot the data using each colormap
+for ax, cmap_name in zip(axes, colormap_names):
+    cmap = matplotlib.colormaps[cmap_name]  # Access the colormap
+    psm = ax.pcolormesh(data, cmap=cmap, rasterized=True, vmin=0, vmax=1)
+    ax.set_title(cmap_name)  # Set the title to the colormap name
+    fig.colorbar(psm, ax=ax)
+
+# Show the plots
+plt.show()
