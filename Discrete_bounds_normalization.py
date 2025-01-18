7b6abe939043e100b895f7a05c217123f52082fa
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-17 18:22:41 -0800

    working with discrete bound normalizations

---
+++ Discrete_bounds_normalization.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib_color import colors 
+
+
+# Create Discrete Bounds  Normalization
+'''colors.BoundaryNorm. This is particularly useful when data needs to be
+ mapped between specified boundaries with linearly distributed colors.
+''' 
+
+X, Y = np.meshgrid(np.linspace(-3, 3, 150), np.linspace(-3, 3, 150))
+Z = (1 + np.sin(Y * 10.)) * X**2
+
+fig, ax = plt.subplots(2, 2, figsize=(7, 6), layout='constrained')
+ax = ax.flatten()
+
+# Default norm:
+pcm = ax[0].pcolormesh(X, Y, Z, cmap='RdBu_r')
+fig.colorbar(pcm, ax=ax[0], orientation='vertical')
+ax[0].set_title('Default norm')
+
+# Even bounds give a contour-like effect:
+bounds = np.linspace(-1.5, 1.5, 7)
+norm = colors.BoundaryNorm(boundaries=bounds, ncolors=256)
+pcm = ax[1].pcolormesh(X, Y, Z, norm=norm, cmap='RdBu_r')
+fig.colorbar(pcm, ax=ax[1], extend='both', orientation='vertical')
+ax[1].set_title('BoundaryNorm: 7 boundaries')
+
+# Bounds may be unevenly spaced:
+bounds = np.array([-0.2, -0.1, 0, 0.5, 1])
+norm = colors.BoundaryNorm(boundaries=bounds, ncolors=256)
+pcm = ax[2].pcolormesh(X, Y, Z, norm=norm, cmap='RdBu_r')
+fig.colorbar(pcm, ax=ax[2], extend='both', orientation='vertical')
+ax[2].set_title('BoundaryNorm: nonuniform')
+
+# With out-of-bounds colors:
+bounds = np.linspace(-1.5, 1.5, 7)
+norm = colors.BoundaryNorm(boundaries=bounds, ncolors=256, extend='both')
+pcm = ax[3].pcolormesh(X, Y, Z, norm=norm, cmap='RdBu_r')
+# The colorbar inherits the "extend" argument from BoundaryNorm.
+fig.colorbar(pcm, ax=ax[3], orientation='vertical')
+ax[3].set_title('BoundaryNorm: extend="both"')
+
+plt.show()
