8104f56e92661472abe186d34c756421bf295aac
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-02-02 20:08:00 -0800

    figure visualization in matplotlib

--- figures_and_backends/figure_visualization.py
+++ figures_and_backends/figure_visualization.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure(figsize=(4, 2), facecolor='lightskyblue',
                  layout='constrained')
-fig.suptitle('A nice Matplotlib Figure')
+fig.suptitle('Sample Figure')
 ax = fig.add_subplot()
 ax.set_title('Axes', loc='left', fontstyle='oblique', fontsize='medium')
 
